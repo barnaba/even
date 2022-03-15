85cd32c3ab628abf9fdf80165549e81270055c56
Barnaba Turek <barnabaturek@gmail.com>
2022-03-15 01:45:43 +0100

    important advice

--- even/include/even.h
+++ even/include/even.h
@@ -1,7 +1,11 @@
 #ifndef EVEN_H
 #define EVEN_H
 
-#define MAX_STACK 50000
+#define MAX_STACK 65536
+
+// for best memory use,if you're going to calculate
+// the evenness of n, MAX_STACK should be sqrt(n)
+
 
 unsigned int even(unsigned int num);
 
